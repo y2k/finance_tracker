657e25651632ac3892bfb8ae2950f3d5f0a0034e
y2k <itwisterlx@gmail.com>
2025-04-11 18:49:26 +0200

    refactoring

--- domain/domain.clj
+++ domain/domain.clj
@@ -1,1 +1,15 @@
 (ns domain)
+
+(defn main []
+  [:row {}
+   [:button {:title "QR"
+             :onclick [:android_gallery:get_image]}]
+   [:button {:title "Settings"
+             :onclick [:chat_ui:update [:button {:title "TEST 2"
+                                                 :onclick nil}]]}]])
+
+(defn main_event [uri]
+  [:android_qr:recognize
+   {:uri uri
+    :callback (fn [xs]
+                [:chat_ui:update [:label {:text (str x)}]])}])
